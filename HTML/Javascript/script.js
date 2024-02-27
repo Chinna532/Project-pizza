82aab0cb789c76b3f083029654f91913458b95d4
Banoth Rajesh <120710438+Chinna532@users.noreply.github.com>
2024-02-27 15:17:23 +0530

    Add files via upload

---
+++ HTML/Javascript/script.js
@@ -0,0 +1,59 @@
+/*
+//output methods
+--console.log("Hello Javascript");
+console.error("This is an Error")
+console.warn("This is warning")
+document.write("This is rajesh")
+document.writeln("nayak banoth")
+window.alert("this is alert")
+window.prompt("this is prompt")
+window.confirm("do you want  to confirm& proceed")--*/
+
+
+
+//by using string literal
+//let x="rajeshnayak"
+//console.log(x)
+
+
+//by using string constructor
+
+/**let y="adward"
+console.log(y.length)
+console.log(y.slice(0,5))
+console.log(y.substring(2))
+console.log(y.substr(0,5))
+console.log(y.trimEnd())
+console.log(y.padStart(18,"banoth "))
+console.log(y.padEnd(18," banoth"))
+
+
+//by using data types
+const x=10;
+document.write(x);
+*/
+let y="Banoth Rajesh Nayak Banotn Chinna";
+console.log(y.slice(1,20))
+console.log(y.length)
+console.log(y.substring(4,12))
+console.log(y.substr(0,12))
+console.log(y.trimStart())
+console.log(y.replace('Banoth','Chinna'))
+console.log(y.replaceAll('Banoth',"Chinna"))
+console.log(y.startsWith('B'))
+console.log(y.endsWith('n'))
+console.log(y.indexOf('e'))
+console.log(y.lastIndexOf('a'))
+console.log(y.toUpperCase())
+console.log(y.toLocaleLowerCase())
+console.log(y.includes('Nayak'))
+
+//let a=Number(prompt('Enter a value'))
+//let b=Number(prompt('Enter b value'))
+//let sum=a+b
+//console.log('sum of ${a} and ${b} is')
+//console.log(sum)
+
+
+let v
+console.log(v)
